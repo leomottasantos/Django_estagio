4735ae945eed2c68ac6952d52f614827429863ae
Leonardo Motta <leonardomottadossantos@gmail.com>
2024-07-16 21:28:25 -0300

    projeto finalizado

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View 
+from django.urls import reverse_lazy
+from .models import Todo
+from django.shortcuts import get_object_or_404, redirect
 
-def todo_list(request):
-    nome = "Leonardo"
-    alunos = ["Aluno um", "Aluno dois", "Aluno tres"]
-    return render(request, "todos/todo_list.html", {"nome": nome, "alunos": alunos})
+
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.mark_has_complete()
+        return redirect("todo_list")      
